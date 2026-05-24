0cba62c24d686c3555ab26e1fd79ed3fd6ba2a39
Arley-Orozco <trabajos001989@gmail.com>
2026-05-23 20:16:59 -0500

    Update asistente.py

--- asistente.py
+++ asistente.py
@@ -1,2 +1,7 @@
-print("Hola, soy tu asistente virtual")
-print("Estoy listo para ayudarte, Arley")
+# Mi asistente virtual - Dia 3
+nombre = "Arley"
+edad = 36
+
+print("Hola " + nombre + ", bienvenido a tu asistente virtual")
+print("Hoy vamos a aprender juntos")
+print("Tu nombre tiene " + str(len(nombre)) + "letras"
