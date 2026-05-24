3dc64176f83ec78b5d60a595e7a3583bac82a0e2
Arley-Orozco <trabajos001989@gmail.com>
2026-05-24 10:25:15 -0500

    Update asistente.py

--- asistente.py
+++ asistente.py
@@ -1,7 +1,18 @@
-# Mi asistente virtual - Dia 3
+# Mi asistente virtual - Dia 4
 nombre = "Arley"
 edad = 36
+hora = 8
 
-print("Hola " + nombre + ", bienvenido a tu asistente virtual")
-print("Hoy vamos a aprender juntos")
-print("Tu nombre tiene " + str(len(nombre)) + "letras"
+# El asiste decide el saludo segun la hora
+if hora < 12:
+  print("Buenos dias " + nombre + "!")
+elif hora <18:
+  print("Buenas tardes " + nombre + "!")
+else:
+  print("Buenas noches " + nombre + "!")
+
+# El asistente decide segun la edad
+if edad >=18:
+ print("Eres mayor de edad, puedes usar todas las funciones")
+else:
+  print("Eres menor de edad")
