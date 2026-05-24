66b8871602c456fa388cf7365c7745ffc6114db1
Arley-Orozco <trabajos001989@gmail.com>
2026-05-24 14:22:14 -0500

    Update asistente.py

--- asistente.py
+++ asistente.py
@@ -1,18 +1,15 @@
-# Mi asistente virtual - Dia 4
+# Mi asistente virtual - Dia 5
 nombre = "Arley"
-edad = 36
-hora = 8
 
-# El asiste decide el saludo segun la hora
-if hora < 12:
-  print("Buenos dias " + nombre + "!")
-elif hora <18:
-  print("Buenas tardes " + nombre + "!")
-else:
-  print("Buenas noches " + nombre + "!")
+# Lista de tareas del dia
+tareas = ["Revisar correos", "Reunion 10am", "Almuerzo", "Ejercicio"]
+print("Hola " + nombre + "! Tus tareas de hoy son:")
+print("---------------------------------")
 
-# El asistente decide segun la edad
-if edad >=18:
- print("Eres mayor de edad, puedes usar todas las funciones")
-else:
-  print("Eres menor de edad")
+#Bucle muestra cada tarea automaticamente
+for tarea  in tareas:
+  print("- " + tarea)
+
+print("--------------------------------")
+print("Total de tareas: " + str(len(tareas)))
+
