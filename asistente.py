0141e929853300c9be228cfe11afac36e1a69811
Arley <trabajos001989@gmail.com>
2026-05-27 19:01:46 -0500

    Dia 6 completado

--- asistente.py
+++ asistente.py
@@ -1,15 +1,23 @@
-# Mi asistente virtual - Dia 5
-nombre = "Arley"
+# Mi asistente virtual - Dia 6
 
-# Lista de tareas del dia
+# Funcion 1: Saludar
+def saludar(nombre, hora):
+    if hora < 12:
+        print("Buenos dias " + nombre + "!")
+    elif hora < 18:
+        print("Buenas tardes " + nombre + "!")
+    else:
+        print("Buenas noches " + nombre + "!")
+
+# Funcion 2: mostrar tareas
+def mostrar_tareas(tareas):
+    print("Tus tareas para hoy son:")
+    print("------------------------------")
+    for tarea in tareas:
+        print("- " + tarea)
+    print("Total: " + str(len(tareas)) + " tareas")
+
+# Usando las funciones
+saludar("Arley", 9)
 tareas = ["Revisar correos", "Reunion 10am", "Almuerzo", "Ejercicio"]
-print("Hola " + nombre + "! Tus tareas de hoy son:")
-print("---------------------------------")
-
-#Bucle muestra cada tarea automaticamente
-for tarea  in tareas:
-  print("- " + tarea)
-
-print("--------------------------------")
-print("Total de tareas: " + str(len(tareas)))
-
+mostrar_tareas(tareas)
