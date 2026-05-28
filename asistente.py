1c437300641460fb088042edfe8c44ffcad8ce8c
Arley <trabajos001989@gmail.com>
2026-05-27 19:48:39 -0500

    Dia 7 completado

--- asistente.py
+++ asistente.py
@@ -1,8 +1,8 @@
-# Mi asistente virtual - Dia 6
+# Mi asistente virtual - Dia 7
 
 # Funcion 1: Saludar
 def saludar(nombre, hora):
-    if hora < 12:
+    if hora <12:
         print("Buenos dias " + nombre + "!")
     elif hora < 18:
         print("Buenas tardes " + nombre + "!")
@@ -12,12 +12,15 @@ def saludar(nombre, hora):
 # Funcion 2: mostrar tareas
 def mostrar_tareas(tareas):
     print("Tus tareas para hoy son:")
-    print("------------------------------")
+    print("----------------------------")
     for tarea in tareas:
         print("- " + tarea)
     print("Total: " + str(len(tareas)) + " tareas")
 
-# Usando las funciones
-saludar("Arley", 9)
+# Dia 7: Pedirle datos al usuario
+nombre = input("Hola! Como te llamas? ")
+hora = int(input("Que hora es? (solo el numero): "))
+
+saludar(nombre, hora)
 tareas = ["Revisar correos", "Reunion 10am", "Almuerzo", "Ejercicio"]
 mostrar_tareas(tareas)
